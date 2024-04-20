2cad0fd3a18fa86e8c4df2e32ca053e7cbf40a8a
roypeco <roy.peco@gmail.com>
2024-04-20 22:39:23 +0900

    add go.sum

--- go-app/main.go
+++ go-app/main.go
@@ -14,5 +14,5 @@ func main() {
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
     })
-    e.Logger.Fatal(e.Start(":3000"))
+    e.Logger.Fatal(e.Start(":8000"))
 }
