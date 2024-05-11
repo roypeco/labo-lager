6adb671a7e8b745b5a41c20bac4ef4c9b74f2645
roypeco <roy.peco@gmail.com>
2024-05-12 00:41:56 +0900

    簡易的なユーザ登録を実装

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,10 +1,21 @@
-import Layout from "@/components/Layout";
+import DrawerAppBar from "@/components/DrawerAppBar";
+import Box from '@mui/material/Box';
+
 
 const Index = () => {
 	return (
-		<Layout>
-			<h1>LaboLager</h1>
-		</Layout>
+		<div>
+			<DrawerAppBar/>
+			<Box
+				sx={{
+					marginTop: 15,
+					display: 'flex',
+					flexDirection: 'column',
+            		alignItems: 'center',
+				}}>
+				<a href="/login">Login Page</a>
+			</Box>
+		</div>
 	);
 };
 
--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,11 +1,119 @@
-import Layout from "@/components/Layout";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import DrawerAppBar from '@/components/DrawerAppBar';
 
-const Ligin = () => {
-	return (
-		<Layout>
-			<h1>Login Form</h1>
-		</Layout>
-	);
-};
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export default Ligin;
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const response = await fetch('http://localhost:8000/register/user', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(Object.fromEntries(data)),
+    })
+    if (!response.ok) {
+      console.log(`HTTP error! status: ${response.status}`);
+    } else{
+      // console.log(response)
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 15,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <DrawerAppBar />
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="ユーザー名"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="pass"
+              label="パスワード"
+              type="pass"
+              id="pass"
+              autoComplete="current-pass"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot pass?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
